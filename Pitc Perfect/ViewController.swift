88feba1e143c2dc79bd74df95de2d8a797a6af4e
vicoociv <Victor@Vico.local>
2015-06-12 13:27:17 -0700

    Initial Commit

---
+++ Pitc Perfect/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Pitc Perfect
+//
+//  Created by Victor Chien on 6/12/15.
+//  Copyright (c) 2015 Victor Chien. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
